6d46bd06c89d625b66aeeb3f700d8693880aa01c
Dobeq <iamwillfischer@gmail.com>
2018-10-28 04:07:18 -0400

    Allows multi-word champ names

--- getid.py
+++ getid.py
@@ -4,5 +4,10 @@ def getChampId(name):
     champs = requests.get("http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json")
     champs = champs.json()
     name = name.lower()
-    name = name[0].upper() + name[1:]
-    return champs["data"][name]["key"]
+    name.split(" ")
+    for word in name:
+        word = word[0].upper() + word[1:]
+    namecapped = ""
+    for word in name:
+        namecapped += word
+    return champs["data"][namecapped]["key"]
